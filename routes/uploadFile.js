1e34ae80fd820dfb8fd79583f4caeb260ef74811
jellyzhou <tangtangshuizhou@gmail.com>
2017-09-06 23:03:08 +0800

    multer进行单个文件上传的练习

--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -2,15 +2,20 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path= require("path");
+var fs = require('fs');
 var formidable = require('formidable');
+
 router.get('/uploadFile', function (req, res, next) {
-	console.log('---------headersInfo', req.headers)
+	// console.log('---------headersInfo', req.headers)
 	var data = {
 		title: '上传文件'
 	}
 	res.render('uploadFile', data)
 })
-	
+
+/************************ 
+  formidable 中间件的使用
+********************************/
 router.post('/upload', function(req, res, next) {
   console.log('开始文件上传....');
    var form = new formidable.IncomingForm();
@@ -51,9 +56,105 @@ router.post('/upload', function(req, res, next) {
       
       //res.end(util.inspect({fields: fields, files: files}));
     });
-
-   
-
 });
+
+/************************ 
+  multer 中间件的使用
+********************************/
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    console.log('+=======++', file)
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+var upload = multer({storage: storage});
+
+var imgFilter = function (req, file, cb) {
+  if (file.mimetype != 'image/jpeg' || file.mimetype != 'image/jpg' || file.mimetype != 'image/gif' || file.mimetype != 'image/png') {
+    // cb(new Error('图片只能jpg/jpeg/png/gif'))  
+    cb(null, false)
+  }
+  else {
+    cb(null, true)
+  }
+}
+
+// upload.fileFilter = imgFilter
+
+// 单文件的上传练习
+router.post('/m_single', upload.single('avatar'), function (req, res, next) {
+  console.log('--------------', req.file)
+  console.log(typeof req.file.mimetype)
+  console.log('------req.file.mimetype', req.file.mimetype)
+  // console.log('------------jpeg', typeof req.file.mimetype == "image\/jpeg")
+  // console.log('------------jpg', typeof req.file.mimetype == "image\/jpg")
+  // console.log('------------gif', typeof req.file.mimetype == "image\/gif")
+  // console.log('------------png', typeof req.file.mimetype == 'image\/png')
+
+  var originalname = req.file.originalname
+  var ext = originalname.substring(originalname.lastIndexOf('.') + 1)
+  console.log('------------jpeg', req.file.mimetype == "image/jpeg", ext == "jpeg")
+  console.log('------------jpg', req.file.mimetype == "image/jpg", ext == "jpg")
+  console.log('------------gif', req.file.mimetype == "image/gif", ext == "gif")
+  console.log('------------png', req.file.mimetype == 'image/png', ext == "png")
+
+
+  if (ext != "jpeg" && ext != "jpg" && ext != "gif" && ext != "png") {
+    res.status(400).send({
+      message: '图片只能jpg/jpeg/png/gif',
+      result: null,
+      success: false,
+      code: -2
+    })
+  }
+  else {
+    res.status(200).send({
+      message: '上传图片成功',
+      success: true,
+      result: [req.file.path],
+      code: 1
+    })
+  }
+})
+
+
+router.post('/m_multipart', upload.fields([{ name: 'file', maxCount: 1 }, {name:'ss', maxCount: 1 }]), function (req, res, next) {
+  console.log('----------req.files', req.files)
+  //   var filesArr = req.files.filter(function (item, index) {
+  //   var originalname = item.file.originalname
+  //   var ext = originalname.substring(originalname.lastIndexOf('.') + 1)
+
+  //   console.log('----------ext')
+  //   if (ext != "jpeg" && ext != "jpg" && ext != "gif" && ext != "png") {
+  //     return false
+  //   }
+  //   else {
+  //     return true
+  //   }
+  // })
+
+  // if (filesArr.length === 0) {
+  //   res.status(400).send({
+  //     message: '图片只能jpg/jpeg/png/gif',
+  //     result: null,
+  //     success: false,
+  //     code: -2
+  //   })
+  // }
+  // else {
+  //   res.status(200).send({
+  //     message: '上传图片成功',
+  //     success: true,
+  //     result: [req.file.path],
+  //     code: 1
+  //   })
+  // }
+})
 
 module.exports = router;
