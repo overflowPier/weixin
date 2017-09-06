e64bf8f7b9b0496e20f6e0d2b23ad07ebba1ef0f
jellyzhou <tangtangshuizhou@gmail.com>
2017-09-06 20:52:28 +0800

    users路由删除

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 // var weixin = require('./routes/weixin');
 var uploadFile = require('./routes/uploadFile');
 
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 // app.use('/weixin', weixin);
 app.use('/', uploadFile);
 
